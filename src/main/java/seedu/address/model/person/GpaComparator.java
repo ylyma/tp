f7baeb461308bdcab18d4f61462c2e2c62a3a94f
ylyma <amylingyuhui@gmail.com>
2023-10-15 01:25:20 +0800

    Add unit tests for SortByGpaCommand

--- src/main/java/seedu/address/model/person/GpaComparator.java
+++ src/main/java/seedu/address/model/person/GpaComparator.java
@@ -2,9 +2,13 @@ package seedu.address.model.person;
 
 import java.util.Comparator;
 
+/**
+ * Compares two persons based on their GPA.
+ */
 public class GpaComparator implements Comparator<Person> {
 	@Override
 	public int compare(Person o1, Person o2) {
 		return (int) ((o2.getGpa().value - o1.getGpa().value) * 100);
 	}
+
 }
