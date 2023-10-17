0fa88e9988c9e2844f66e9f164d2fbc7cfc80736
LHeng1 <limhengyi1@gmail.com>
2023-10-17 10:59:42 +0800

    Add skeletal compare logic to codebase

---
+++ src/main/java/seedu/address/logic/commands/CompareCommand.java
@@ -0,0 +1,51 @@
+package seedu.address.logic.commands;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.person.Person;
+
+import static java.util.Objects.requireNonNull;
+
+public class CompareCommand extends Command {
+
+    public static final String COMMAND_WORD = "compare";
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Compares two applicants side by side.\n"
+            + "Parameters: INDEX1 and INDEX2 (must be a positive integers)\n"
+            + "Example: " + COMMAND_WORD + " 1 2";
+
+    private final Index index1;
+    private final Index index2;
+
+    public CompareCommand(Index index1, Index index2) {
+        requireNonNull(index1);
+        requireNonNull(index2);
+
+        this.index1 = index1;
+        this.index2 = index2;
+    }
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+
+        if (index1.equals(index2)) {
+            throw new CommandException("Error: Please provide distinct indices. "
+                    + "You cannot compare the same applicant.");
+        }
+
+        try {
+            Person person1 = model.getFilteredPersonList().get(index1.getZeroBased());
+            Person person2 = model.getFilteredPersonList().get(index2.getZeroBased());
+
+            // TODO: Implement the comparison and display logic here
+
+            return new CommandResult("Comparison successful");
+        } catch (IndexOutOfBoundsException e) {
+            throw new CommandException("Error: One or both of the specified applicants"
+                    + "were not found in the list.");
+        }
+    }
+}
+
