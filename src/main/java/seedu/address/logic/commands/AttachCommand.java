26224e2f7fb99168025d58795d9351db9ba1b704
Ravern Koh <ravernkoh@gmail.com>
2023-10-19 16:35:51 +0800

    Fix checkstyle

--- src/main/java/seedu/address/logic/commands/AttachCommand.java
+++ src/main/java/seedu/address/logic/commands/AttachCommand.java
@@ -4,7 +4,6 @@ import static java.util.Objects.requireNonNull;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_FILE;
 
 import java.io.IOException;
-import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
