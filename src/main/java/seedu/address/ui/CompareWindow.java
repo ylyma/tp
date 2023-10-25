097786180d12eb1749176c8a75b2ea007f1ac3d9
LHeng1 <limhengyi1@gmail.com>
2023-10-25 20:09:32 +0800

    Add FXML file for compare function pop-up window

--- src/main/java/seedu/address/ui/CompareWindow.java
+++ src/main/java/seedu/address/ui/CompareWindow.java
@@ -1,19 +1,43 @@
 package seedu.address.ui;
 
-import javafx.geometry.HPos;
-import javafx.geometry.Insets;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.scene.layout.GridPane;
-import javafx.scene.text.Font;
 import javafx.stage.Stage;
+import seedu.address.MainApp;
 import seedu.address.model.person.Gpa;
 import seedu.address.model.person.Name;
 import seedu.address.model.person.Person;
 
+import java.io.IOException;
+
 public class CompareWindow extends Stage {
 
+    @FXML
+    private Label person1Name;
+    @FXML
+    private Label person1Gpa;
+    @FXML
+    private Label person2Name;
+    @FXML
+    private Label person2Gpa;
+    @FXML
+    private Label nameLabel;
+    @FXML
+    private Label gpaLabel;
+    @FXML
+    private Label compareMessageLabel;
+
     public CompareWindow(Person person1, Person person2) {
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("/view/CompareWindow.fxml"));
+        fxmlLoader.setController(this);
+        try {
+            fxmlLoader.load();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
         setTitle("Comparison Result");
 
         String compareMessage;
@@ -22,13 +46,10 @@ public class CompareWindow extends Stage {
         Name name2 = person2.getName();
         Gpa gpa2 = person2.getGpa();
 
-        Label person1Name = new Label(name1.toString());
-        Label person1Gpa = new Label(gpa1.toString());
-        Label person2Name = new Label(name2.toString());
-        Label person2Gpa = new Label(gpa2.toString());
-        Label nameLabel = new Label("Name");
-        Label gpaLabel = new Label("GPA");
-
+        person1Name.setText(name1.toString());
+        person1Gpa.setText(gpa1.toString());
+        person2Name.setText(name2.toString());
+        person2Gpa.setText(gpa2.toString());
 
         if (gpa1.compareTo(gpa2) == 0) {
             compareMessage = "They have the same GPA, do look out for other criteria!";
@@ -38,33 +59,11 @@ public class CompareWindow extends Stage {
             compareMessage = name2 + " has a higher GPA!";
         }
 
-        Label compareMessageLabel = new Label(compareMessage);
-        compareMessageLabel.setFont(Font.font(40));
+        compareMessageLabel.setText(compareMessage);
 
-        GridPane gridPane = new GridPane();
-        gridPane.setHgap(10);
-        gridPane.setVgap(10);
-        gridPane.setPadding(new Insets(10, 10, 10, 10));
-
-        gridPane.add(person1Name, 0, 0);
-        gridPane.add(nameLabel, 1, 0);
-        gridPane.add(person2Name, 2, 0);
-
-        gridPane.add(person1Gpa, 0, 1);
-        gridPane.add(gpaLabel, 1, 1);
-        gridPane.add(person2Gpa, 2, 1);
-
-        gridPane.add(compareMessageLabel, 1, 2);
-
-        GridPane.setHalignment(nameLabel, HPos.CENTER);
-        GridPane.setHalignment(gpaLabel, HPos.CENTER);
-
-        Scene scene = new Scene(gridPane);
-
+        Scene scene = new Scene(fxmlLoader.getRoot());
         setWidth(900);
         setHeight(600);
-
         setScene(scene);
     }
 }
-
