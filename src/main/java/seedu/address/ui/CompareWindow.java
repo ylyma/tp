1146a74f688a94f5bf74893ff4fa01c5626d89f3
LHeng1 <limhengyi1@gmail.com>
2023-10-26 02:03:13 +0800

    Improve compare command UI design

--- src/main/java/seedu/address/ui/CompareWindow.java
+++ src/main/java/seedu/address/ui/CompareWindow.java
@@ -1,19 +1,27 @@
 package seedu.address.ui;
 
+import java.io.IOException;
+
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import seedu.address.MainApp;
 import seedu.address.model.person.Gpa;
-import seedu.address.model.person.Name;
 import seedu.address.model.person.Person;
 
-import java.io.IOException;
-
+/**
+ * The Compare Pop-up Window. Provides a separate JavaFX stage when a "compare" command is inputted.
+ * This window is used to display and compare the details of two applicants,
+ * including student number, name, and their relevant achievements.
+ */
 public class CompareWindow extends Stage {
-
+    @FXML
+    private Label person1StuNum;
+    @FXML
+    private Label person2StuNum;
     @FXML
     private Label person1Name;
     @FXML
@@ -27,8 +35,16 @@ public class CompareWindow extends Stage {
     @FXML
     private Label gpaLabel;
     @FXML
-    private Label compareMessageLabel;
+    private Rectangle gpa1Highlight;
+    @FXML
+    private Rectangle gpa2Highlight;
 
+    /**
+     * Creates a new instance of the CompareWindow with information about two persons to be compared.
+     *
+     * @param person1 The first person for comparison.
+     * @param person2 The second person for comparison.
+     */
     public CompareWindow(Person person1, Person person2) {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("/view/CompareWindow.fxml"));
         fxmlLoader.setController(this);
@@ -40,26 +56,22 @@ public class CompareWindow extends Stage {
 
         setTitle("Comparison Result");
 
-        String compareMessage;
-        Name name1 = person1.getName();
         Gpa gpa1 = person1.getGpa();
-        Name name2 = person2.getName();
         Gpa gpa2 = person2.getGpa();
 
-        person1Name.setText(name1.toString());
+        person1StuNum.setText(person1.getStudentNumber().toString());
+        person1Name.setText(person1.getName().toString());
         person1Gpa.setText(gpa1.toString());
-        person2Name.setText(name2.toString());
+
+        person2StuNum.setText(person2.getStudentNumber().toString());
+        person2Name.setText(person2.getName().toString());
         person2Gpa.setText(gpa2.toString());
 
-        if (gpa1.compareTo(gpa2) == 0) {
-            compareMessage = "They have the same GPA, do look out for other criteria!";
-        } else if (gpa1.compareTo(gpa2) > 0) {
-            compareMessage = name1 + " has a higher GPA!";
-        } else {
-            compareMessage = name2 + " has a higher GPA!";
+        if (gpa1.compareTo(gpa2) > 0) {
+            gpa1Highlight.setOpacity(0.33);
+        } else if (gpa1.compareTo(gpa2) < 0) {
+            gpa2Highlight.setOpacity(0.33);
         }
-
-        compareMessageLabel.setText(compareMessage);
 
         Scene scene = new Scene(fxmlLoader.getRoot());
         setWidth(900);
