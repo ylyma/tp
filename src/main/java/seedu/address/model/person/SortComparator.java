5cd67c94a1955b343485f0a55244915de3613b93
Ravern Koh <ravernkoh@gmail.com>
2023-10-29 13:11:19 +0800

    Update fields in sort command

--- src/main/java/seedu/address/model/person/SortComparator.java
+++ src/main/java/seedu/address/model/person/SortComparator.java
@@ -1,9 +1,12 @@
 package seedu.address.model.person;
 
+import static seedu.address.logic.parser.CliSyntax.FIELD_COMMENT;
 import static seedu.address.logic.parser.CliSyntax.FIELD_EMAIL;
 import static seedu.address.logic.parser.CliSyntax.FIELD_GPA;
+import static seedu.address.logic.parser.CliSyntax.FIELD_INTERVIEW_SCORE;
 import static seedu.address.logic.parser.CliSyntax.FIELD_NAME;
 import static seedu.address.logic.parser.CliSyntax.FIELD_PHONE;
+import static seedu.address.logic.parser.CliSyntax.FIELD_PREVIOUS_GRADE;
 import static seedu.address.logic.parser.CliSyntax.FIELD_STUDENT_NUMBER;
 import static seedu.address.logic.parser.CliSyntax.FIELD_TAGS;
 
@@ -28,6 +31,28 @@ public class SortComparator implements Comparator<Person> {
             return p1.getStudentNumber().value.compareTo(p2.getStudentNumber().value);
         case FIELD_GPA:
             return (int) ((p2.getGpa().value - p1.getGpa().value) * 100);
+        case FIELD_PREVIOUS_GRADE:
+            return p1.getPreviousGrade().grade.compareTo(p2.getPreviousGrade().grade);
+        case FIELD_INTERVIEW_SCORE:
+            if (p1.getInterviewScore().isPresent() && p2.getInterviewScore().isPresent()) {
+                return Double.compare(p2.getInterviewScore().get().value, p1.getInterviewScore().get().value);
+            } else if (p1.getInterviewScore().isPresent()) {
+                return -1;
+            } else if (p2.getInterviewScore().isPresent()) {
+                return 1;
+            } else {
+                return 0;
+            }
+        case FIELD_COMMENT:
+            if (p1.getComment().isPresent() && p2.getComment().isPresent()) {
+                return p1.getComment().get().comment.compareTo(p2.getComment().get().comment);
+            } else if (p1.getComment().isPresent()) {
+                return -1;
+            } else if (p2.getComment().isPresent()) {
+                return 1;
+            } else {
+                return 0;
+            }
         case FIELD_PHONE:
             return p1.getPhone().value.compareTo(p2.getPhone().value);
         case FIELD_EMAIL:
