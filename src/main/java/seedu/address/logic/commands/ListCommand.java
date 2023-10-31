5ccfa751f77102801aaacedd755bb383a71e380e
ylyma <amylingyuhui@gmail.com>
2023-10-31 20:20:42 +0800

    Generalise function of list command to handle new fields

--- src/main/java/seedu/address/logic/commands/ListCommand.java
+++ src/main/java/seedu/address/logic/commands/ListCommand.java
@@ -2,7 +2,14 @@ package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 
+import java.util.Objects;
+import java.util.function.Predicate;
+
+import seedu.address.commons.util.ToStringBuilder;
+import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
+import seedu.address.model.person.ListPredicate;
+import seedu.address.model.person.Person;
 
 /**
  * Lists all applicants in the applicant list to the user.
@@ -10,13 +17,55 @@ import seedu.address.model.Model;
 public class ListCommand extends Command {
 
     public static final String COMMAND_WORD = "list";
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Lists all applicants filtered by the given field.\n"
+            + "Parameters: FIELD_NAME\n"
+            + "Example: " + COMMAND_WORD + " hidden";
 
-    public static final String MESSAGE_SUCCESS = "Listed all applicants.";
+    public static final String MESSAGE_SUCCESS = "Listed applicants.";
+    public static final String MESSAGE_EMPTY_LIST = "No applicants to list.";
+    public final String fieldName;
+
+    public ListCommand(String fieldName) {
+        this.fieldName = fieldName;
+    }
+
+    public ListCommand() {
+        this.fieldName = "";
+    }
 
     @Override
-    public CommandResult execute(Model model) {
+    public CommandResult execute(Model model) throws CommandException {
         requireNonNull(model);
-        model.updateFilteredPersonList(Model.PREDICATE_SHOW_ALL_UNHIDDEN_PERSONS);
+
+        if (fieldName.isEmpty()) {
+            model.updateFilteredPersonList(Model.PREDICATE_SHOW_ALL_PERSONS);
+        } else {
+            Predicate<Person> predicate = new ListPredicate(fieldName, true);
+            model.updateFilteredPersonList(predicate);
+        }
+        if (model.getFilteredPersonList().isEmpty()) {
+            throw new CommandException(MESSAGE_EMPTY_LIST);
+        }
+
+
         return new CommandResult(MESSAGE_SUCCESS);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+        // instanceof handles nulls
+        if (!(other instanceof ListCommand)) {
+            return false;
+        }
+        ListCommand otherListCommand = (ListCommand) other;
+        return fieldName.equals(otherListCommand.fieldName);
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this).add("fieldName", fieldName).toString();
     }
 }
---
+++ src/main/java/seedu/address/model/person/ListPredicate.java
@@ -0,0 +1,31 @@
+package seedu.address.model.person;
+
+import static seedu.address.logic.parser.CliSyntax.FIELD_BOOKMARKED;
+import static seedu.address.logic.parser.CliSyntax.FIELD_HIDDEN;
+
+import java.util.function.Predicate;
+
+import seedu.address.logic.commands.ListCommand;
+
+/**
+ * Tests that a {@code Person}'s specified field matches the boolean given.
+ */
+public class ListPredicate implements Predicate<Person> {
+    private final String fieldName;
+    private final boolean filter;
+    public ListPredicate(String fieldName, boolean filter) {
+        this.fieldName = fieldName;
+        this.filter = filter;
+    }
+    @Override
+    public boolean test(Person person) {
+        switch (fieldName) {
+        case FIELD_HIDDEN:
+            return person.getIsHidden().value == filter;
+        case FIELD_BOOKMARKED:
+            return person.getIsBookmarked().value == filter;
+        default:
+            return false;
+        }
+    }
+}
