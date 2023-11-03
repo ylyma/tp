9b72a09f20b43e1b9226042e53e1a38103b9d4ed
LHeng1 <limhengyi1@gmail.com>
2023-11-03 13:56:46 +0800

    Complete compare GUI

--- src/main/java/seedu/address/ui/CompareWindow.java
+++ src/main/java/seedu/address/ui/CompareWindow.java
@@ -1,6 +1,7 @@
 package seedu.address.ui;
 
 import java.io.IOException;
+import java.util.Optional;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -10,7 +11,9 @@ import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import seedu.address.MainApp;
 import seedu.address.model.person.Gpa;
+import seedu.address.model.person.InterviewScore;
 import seedu.address.model.person.Person;
+import seedu.address.model.person.PreviousGrade;
 
 /**
  * The Compare Pop-up Window. Provides a separate JavaFX stage when a "compare" command is inputted.
@@ -21,23 +24,39 @@ public class CompareWindow extends Stage {
     @FXML
     private Label person1StuNum;
     @FXML
-    private Label person2StuNum;
-    @FXML
     private Label person1Name;
     @FXML
     private Label person1Gpa;
+    @FXML
+    private Rectangle gpa1Highlight;
+    @FXML
+    private Label person1IScore;
+    @FXML
+    private Rectangle IScore1Highlight;
+    @FXML
+    private Label person1MGrade;
+    @FXML
+    private Rectangle MGrade1Highlight;
+    @FXML
+    private Label person1Comment;
+    @FXML
+    private Label person2StuNum;
     @FXML
     private Label person2Name;
     @FXML
     private Label person2Gpa;
     @FXML
-    private Label nameLabel;
-    @FXML
-    private Label gpaLabel;
-    @FXML
-    private Rectangle gpa1Highlight;
-    @FXML
     private Rectangle gpa2Highlight;
+    @FXML
+    private Label person2IScore;
+    @FXML
+    private Rectangle IScore2Highlight;
+    @FXML
+    private Label person2MGrade;
+    @FXML
+    private Rectangle MGrade2Highlight;
+    @FXML
+    private Label person2Comment;
 
     /**
      * Creates a new instance of the CompareWindow with information about two persons to be compared.
@@ -56,21 +75,78 @@ public class CompareWindow extends Stage {
 
         setTitle("Comparison Result");
 
+        //person1 stuff
         Gpa gpa1 = person1.getGpa();
+        PreviousGrade mGrade1 = person1.getPreviousGrade();
+        InterviewScore interviewScore1 = person1.getInterviewScore().get();
+        String interviewScoreString1;
+
+        if (person1.getInterviewScore().isPresent()) {
+            interviewScoreString1 = interviewScore1.toString();
+        } else {
+            interviewScoreString1 = "-";
+        }
+
+        String comment1;
+        if (person1.getComment().isPresent()) {
+            String commentString1 = person1.getComment().toString();
+            comment1 = commentString1.substring("Optional[".length(),
+                    commentString1.length() - 1);
+        } else {
+            comment1 = "-";
+        }
+
+        //person2 stuff
         Gpa gpa2 = person2.getGpa();
+        PreviousGrade mGrade2 = person2.getPreviousGrade();
+        InterviewScore interviewScore2 = person2.getInterviewScore().get();
+        String interviewScoreString2;
+
+        if (person1.getInterviewScore().isPresent()) {
+            interviewScoreString2 = interviewScore2.toString();
+        } else {
+            interviewScoreString2 = "-";
+        }
+
+        String comment2;
+        if (person2.getComment().isPresent()) {
+            String commentString2 = person2.getComment().toString();
+            comment2 = commentString2.substring("Optional[".length(),
+                    commentString2.length() - 1);
+        } else {
+            comment2 = "-";
+        }
 
         person1StuNum.setText(person1.getStudentNumber().toString());
         person1Name.setText(person1.getName().toString());
         person1Gpa.setText(gpa1.toString());
+        person1IScore.setText(interviewScoreString1);
+        person1MGrade.setText(mGrade1.toString());
+        person1Comment.setText(comment1);
 
         person2StuNum.setText(person2.getStudentNumber().toString());
         person2Name.setText(person2.getName().toString());
         person2Gpa.setText(gpa2.toString());
+        person2IScore.setText(interviewScoreString2);
+        person2MGrade.setText(mGrade2.toString());
+        person2Comment.setText(comment2);
 
         if (gpa1.compareTo(gpa2) > 0) {
             gpa1Highlight.setOpacity(0.33);
         } else if (gpa1.compareTo(gpa2) < 0) {
             gpa2Highlight.setOpacity(0.33);
+        }
+
+        if (interviewScore1.compareTo(interviewScore2) > 0) {
+            IScore1Highlight.setOpacity(0.33);
+        } else if (interviewScore1.compareTo(interviewScore2) < 0) {
+            IScore2Highlight.setOpacity(0.33);
+        }
+
+        if (mGrade1.compareTo(mGrade2) < 0) {
+            MGrade1Highlight.setOpacity(0.33);
+        } else if (mGrade1.compareTo(mGrade2) > 0) {
+            MGrade2Highlight.setOpacity(0.33);
         }
 
         Scene scene = new Scene(fxmlLoader.getRoot());
